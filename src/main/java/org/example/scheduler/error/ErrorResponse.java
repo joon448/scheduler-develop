8c955360635e395d4fb64be173493cc6b42733b8
joon448 <joonjdy448@gmail.com>
2025-08-08 16:05:23 +0900

    Feat: Lv5: CustomException및 GlobalExceptionHandler 추가

---
+++ src/main/java/org/example/scheduler/error/ErrorResponse.java
@@ -0,0 +1,28 @@
+package org.example.scheduler.error;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String errorCode;
+    private String message;
+    private String path;
+    private LocalDateTime timestamp;
+    private Map<String, String> errors;
+
+    public static ErrorResponse of(HttpStatusCode status, ErrorCode errorCode, String path, Map<String, String> errors) {
+        return new ErrorResponse(status.value(), errorCode.getCode(), errorCode.getMessage(), path, LocalDateTime.now(), errors);
+    }
+    public static ErrorResponse of(HttpStatusCode status, String errorCode, String message, String path) {
+        return new ErrorResponse(status.value(), errorCode, message, path, LocalDateTime.now(), null);
+    }
+}
---
+++ src/main/java/org/example/scheduler/error/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package org.example.scheduler.error;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentValidation(MethodArgumentNotValidException ex, HttpServletRequest req) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,                                  // 어느 필드가
+                        DefaultMessageSourceResolvable::getDefaultMessage,     // 무슨 메시지로 실패했는지
+                        (a, b) -> a,                             // 같은 필드는 첫 메시지 우선
+                        LinkedHashMap::new                                     // 순서 유지
+                ));
+
+        ErrorResponse body = ErrorResponse.of(HttpStatus.BAD_REQUEST, ErrorCode.VALIDATION_FAILED, req.getRequestURI(), errors);
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex, HttpServletRequest req) {
+        ErrorCode code = ex.getErrorCode();
+        ErrorResponse body = ErrorResponse.of(code.getStatus(), code.getCode(), code.getMessage(), req.getRequestURI());
+        return ResponseEntity.status(code.getStatus()).body(body);
+    }
+}
