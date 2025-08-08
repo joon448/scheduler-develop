80f590291427a0cb60dc4b94e86ee798d24b5d60
joon448 <joonjdy448@gmail.com>
2025-08-08 16:10:21 +0900

    Chore: Lv5: ErrorCode 수정 및 정리

--- src/main/java/org/example/scheduler/error/ErrorCode.java
+++ src/main/java/org/example/scheduler/error/ErrorCode.java
@@ -8,16 +8,18 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SYS-500", "서버 내부 오류가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "SYS-400","잘못된 요청입니다."),
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VAL-001","유효성 검증에 실패했습니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH-401","인증이 필요합니다."),
-    FORBIDDEN_NOT_OWNER(HttpStatus.FORBIDDEN, "AUTH-403", "접근 권한이 없습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-404", "존재하지 않는 사용자입니다."),
-    DUPLICATE_USER(HttpStatus.BAD_REQUEST, "USER-001", "이미 가입된 사용자입니다."),
-    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCH-404", "존재하지 않는 일정입니다."),
-    COMMENT_LIMIT_EXCEED(HttpStatus.BAD_REQUEST, "CMT-400","하나의 일정에 댓글은 최대 10개까지 등록할 수 있습니다."),
-    PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "AUTH-401", "비밀번호가 올바르지 않습니다.");
 
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VAL-001","유효성 검증에 실패했습니다."),
+
+    PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "AUTH-401", "비밀번호가 올바르지 않습니다."),
+    FORBIDDEN_NOT_OWNER(HttpStatus.FORBIDDEN, "AUTH-403", "접근 권한이 없습니다."),
+
+    DUPLICATE_USER(HttpStatus.BAD_REQUEST, "USER-001", "이미 가입된 사용자입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-404", "존재하지 않는 사용자입니다."),
+
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCH-404", "존재하지 않는 일정입니다."),
+
+    COMMENT_LIMIT_EXCEED(HttpStatus.BAD_REQUEST, "CMT-400","하나의 일정에 댓글은 최대 10개까지 등록할 수 있습니다.");
 
     private final HttpStatus status;
     private final String code;
