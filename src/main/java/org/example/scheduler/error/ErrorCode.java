154470f8bcde8d6daa9c2f86028912ae438f9a2d
joon448 <joonjdy448@gmail.com>
2025-08-12 09:54:11 +0900

    Style: ErrorCode 코드 수정

--- src/main/java/org/example/scheduler/error/ErrorCode.java
+++ src/main/java/org/example/scheduler/error/ErrorCode.java
@@ -12,14 +12,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SYS-500", "서버 내부 오류가 발생했습니다."),
 
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VAL-001","유효성 검증에 실패했습니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VAL-400","유효성 검증에 실패했습니다."),
 
     PASSWORD_SAME_AS_OLD(HttpStatus.BAD_REQUEST, "AUTH-400","새 비밀번호가 기존 비밀번호와 동일합니다."),
     PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "AUTH-401", "비밀번호가 올바르지 않습니다."),
     FORBIDDEN_NOT_OWNER(HttpStatus.FORBIDDEN, "AUTH-403", "접근 권한이 없습니다."),
 
-    DUPLICATE_USER(HttpStatus.BAD_REQUEST, "USER-001", "이미 가입된 사용자입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-404", "존재하지 않는 사용자입니다."),
+    DUPLICATE_USER(HttpStatus.CONFLICT, "USER-409", "이미 가입된 사용자입니다."),
 
     INVALID_PAGING_PARAM(HttpStatus.BAD_REQUEST, "SCH-400", "page/size 파라미터가 올바르지 않습니다. size는 최대 100까지 가능합니다."),
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "SCH-404", "존재하지 않는 일정입니다."),
